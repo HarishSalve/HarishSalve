03df25c5b3e36ac560a747b2ef3f2ceeaa13ff56
SalveHarish <harish.salve@excellarate.com>
2023-04-19 11:46:08 +0530

    First CPP assignment

---
+++ Assignment1.cpp
@@ -0,0 +1,172 @@
+#include<stdio.h>
+#include<iostream>
+
+//  class Parent {
+//     public:
+//     Parent(){
+//         std::cout<<"Parent constructor\n";
+
+//     }
+//     public: int a, b;
+//     public: void show(){
+//         std::cout<<"The Value of a is = " <<a<< " and b is = " <<b;
+//     }
+// };
+
+
+class Parent_A{
+    public:
+    Parent_A(){
+        std::cout<<"Parent A constructor\n";
+    }
+    ~Parent_A(){
+        std::cout<<"Destructor of A \n";
+    }
+
+    
+};
+
+class B : public Parent_A{
+    public:
+    B(){
+        std::cout<<"Child B of Parent A\n";
+    }
+    
+    
+};
+
+class C : public Parent_A{
+    public:
+    C(){
+        std::cout<<"Child C of Parent A\n";
+    }
+};
+
+class D : public B{
+    public:
+    D(){
+        std::cout<<"Child D of Parent B\n";
+    }
+};
+
+class E : public B{
+    public:
+    E(){
+        std::cout<<"Child E of Parent B\n";
+    }
+};
+
+class F : public C{
+    public:
+    F(){
+        std::cout<<"Child F of Parent C\n";
+    }
+};
+
+class G : public C{
+    public:
+    G(){
+        std::cout<<"Child G of Parent C\n";
+    }
+};
+
+class H : public D, public E{
+    public:
+    H(){
+        std::cout<<"Child H of Parent D & Parent E \n";
+    }
+};
+
+class I : public F, public G{
+    public:
+    I(){
+        std::cout<<"Child I of Parent F & Parent G \n";
+    }
+};
+
+class J : public H, public I{
+    public:
+    J(){
+        std::cout<<"Child J of Parent H & Parent I \n";
+    }
+};
+
+int main(){
+
+    // Parent_A a;
+    // B b;
+    // C c;
+    J Obj;
+
+
+
+
+
+    // Parent obj;
+    // obj.a = 10;
+    // obj.b = 20;
+
+    // obj.show();
+    // std::cout<<"\nFirst Assignment";
+}
+
+
+
+
+// #include <iostream>
+
+// class A
+// {
+// public:
+//     A() { std::cout << "I am constructor of A \n"; }
+// };
+// class B : public A
+// {
+// public:
+//     B() { std::cout << "I am constructoe of B \n"; }
+// };
+// class D : public B
+// {
+// public:
+//     D() { std::cout << "I am construnctor of D\n"; }
+// };
+// class E : public B
+// {
+// public:
+//     E() { std::cout << "I am construnctor of E \n"; }
+// };
+// class H : public E, public D
+// {
+// public:
+//     H() { std::cout << "I am construnctor of H \n"; }
+// };
+// class C : public A
+// {
+// public:
+//     C() { std::cout << "I am constructor of C \n"; }
+// };
+// class F : public C
+// {
+// public:
+//     F() { std::cout << "I am construnctor of F\n"; }
+// };
+// class G : public C
+// {
+// public:
+//     G() { std::cout << " I am construnctor of G \n"; }
+// };
+// class I : public F, public G
+// {
+// public:
+//     I() { std::cout << "I am construnctor of I \n"; }
+// };
+// class J : public H, public I
+// {
+// public:
+//     J() { std::cout << "I am construnctor of J\n"; }
+// };
+// int main()
+// {
+//     J j;
+//     return 0;
+// }
